c2d0cfa9af3edbe164b25497127373b3b2d25804
Yulei Liu <yulei.liu@gmail.com>
2021-08-31 11:22:02 +1000

    proper fix on fileset operations

--- applications.tf
+++ applications.tf
@@ -1,6 +1,6 @@
 locals {
   # Take a directory of JSON files, read each one and bring them in to Terraform's native data set
-  inputappvars = [ for f in fileset(path.module, "applications/[app]*.json") : jsondecode(file(f)) ]
+  inputappvars = [ for f in fileset(path.module, "applications/{app}*.json") : jsondecode(file(f)) ]
   # Take that data set and format it so that it can be used with the for_each command by converting it to a map where each top level key is a unique identifier.
   # In this case I am using the name key from my example JSON files
   inputappmap = {for app in toset(local.inputappvars): app.appid => app}
