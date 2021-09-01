afe386ff38f0209634d5d53079fa15f06c8074a1
Yulei Liu <yulei.liu@gmail.com>
2021-09-01 15:48:38 +1000

    add two roles for aws secrets engine

--- main.tf
+++ main.tf
@@ -110,6 +110,46 @@ resource "vault_aws_secret_backend" "aws" {
 }
 
 
+resource "vault_aws_secret_backend_role" "iam_manager" {
+  backend = vault_aws_secret_backend.aws.path
+  name    = "iam_manager"
+  credential_type = "iam_user"
+
+  policy_document = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "iam:*",
+      "Resource": "*"
+    }
+  ]
+}
+EOT
+}
+
+
+resource "vault_aws_secret_backend_role" "s3_manager" {
+  backend = vault_aws_secret_backend.aws.path
+  name    = "s3_manager"
+  credential_type = "iam_user"
+
+  policy_document = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:*",
+      "Resource": "*"
+    }
+  ]
+}
+EOT
+}
+
+
 //Audit device
 resource "vault_audit" "auditlog" {
   type = "file"
