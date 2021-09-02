26caa07bd7370d86c1ace5eb27e189694adb0eec
Yulei Liu <yulei.liu@gmail.com>
2021-09-02 10:06:36 +1000

    synatx

--- applications.tf
+++ applications.tf
@@ -1,6 +1,6 @@
 locals {
   # Take a directory of YAML files, read each one that matches naming pattern and bring them in to Terraform's native data set
-  inputappvars = [for f in fileset(path.module, "applications/{app}*.yaml") : jsondecode(file(f))]
+  inputappvars = [for f in fileset(path.module, "applications/{app}*.yaml") : yamldecode(file(f))]
   # Take that data set and format it so that it can be used with the for_each command by converting it to a map where each top level key is a unique identifier.
   # In this case I am using the appid key from my example YAML files
   inputappmap = { for app in toset(local.inputappvars) : app.appid => app }
