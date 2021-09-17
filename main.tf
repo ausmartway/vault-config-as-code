2bee4cfecbc64f6dd2e7f4e810a6de88bfe8f3aa
Yulei Liu <yulei.liu@gmail.com>
2021-09-17 12:58:42 +1000

    disable azure role

--- main.tf
+++ main.tf
@@ -60,17 +60,17 @@ resource "vault_azure_secret_backend" "azure" {
   environment     = "AzurePublicCloud"
 }
 
-resource "vault_azure_secret_backend_role" "generated_role" {
-  backend                     = vault_azure_secret_backend.azure.path
-  role                        = "generated_role"
-  ttl                         = 300
-  max_ttl                     = 600
+# resource "vault_azure_secret_backend_role" "generated_role" {
+#   backend                     = vault_azure_secret_backend.azure.path
+#   role                        = "generated_role"
+#   ttl                         = 300
+#   max_ttl                     = 600
 
-  azure_roles {
-    role_name = "Reader"
-    scope =  "/subscriptions/${var.subscription_id}/resourceGroups/azure-vault-group"
-  }
-}
+#   azure_roles {
+#     role_name = "Reader"
+#     scope =  "/subscriptions/${var.subscription_id}/resourceGroups/azure-vault-group"
+#   }
+# }
 
 
 //Approle auth method
