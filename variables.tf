05c833929f88c63c1a26d7028aa37ed638244c04
Yulei Liu <yulei.liu@gmail.com>
2022-03-02 17:17:56 +1100

    disable azure

--- variables.tf
+++ variables.tf
@@ -3,22 +3,22 @@ variable "vault_url" {
   default     = "http://vault.yulei.aws.hashidemos.io:8200/"
 }
 
-variable "azure_tanent_id" {
-  type        = string
-  description = "Azure tanent id"
-}
+# variable "azure_tanent_id" {
+#   type        = string
+#   description = "Azure tanent id"
+# }
 
-variable "azure_client_id" {
-  type        = string
-  description = "Azure client id"
-}
+# variable "azure_client_id" {
+#   type        = string
+#   description = "Azure client id"
+# }
 
-variable "azure_client_secret" {
-  type        = string
-  description = "Azure client secret"
-}
+# variable "azure_client_secret" {
+#   type        = string
+#   description = "Azure client secret"
+# }
 
-variable "azure_subscription_id" {
-  type        = string
-  description = "Azure subscription id"
-}
+# variable "azure_subscription_id" {
+#   type        = string
+#   description = "Azure subscription id"
+# }
