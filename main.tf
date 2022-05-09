9909036067b6979acfa1ffc98ca834e5c68e459b
Yulei Liu <yulei.liu@gmail.com>
2022-05-09 12:42:45 +1000

    reable github auth

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-# //github auth backend, as long as you belong to the hashicorp orgnisation, you will be able to login to Vault and get super user previlige using your personal github token.
-# resource "vault_github_auth_backend" "hashicorp" {
-#   organization   = "hashicorp"
-#   token_policies = ["super-user"]
-# }
+//github auth backend, as long as you belong to the hashicorp orgnisation, you will be able to login to Vault and get super user previlige using your personal github token.
+resource "vault_github_auth_backend" "hashicorp" {
+  organization   = "hashicorp"
+  token_policies = ["super-user"]
+}
 
 resource "vault_policy" "super-user" {
   name   = "super-user"
