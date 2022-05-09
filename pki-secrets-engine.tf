14e35a983ef8629b065a9069544a22ac9a937a55
Yulei Liu <yulei.liu@gmail.com>
2022-05-09 13:10:54 +1000

    disable pki

---
+++ pki-secrets-engine.tf
@@ -0,0 +1,61 @@
+# // //pki root CA secret engine
+# resource "vault_mount" "pki_root" {
+#   path                      = "pki_root"
+#   type                      = "pki"
+#   default_lease_ttl_seconds = 3600 * 24 * 31 * 13     //13 Months
+#   max_lease_ttl_seconds     = 3600 * 24 * 31 * 12 * 3 //3 Years
+# }
+# resource "vault_pki_secret_backend_root_cert" "self-signing-cert" {
+#   backend              = vault_mount.pki_root.path
+#   type                 = "internal"
+#   common_name          = "Root CA"
+#   ttl                  = "315360000"
+#   format               = "pem"
+#   private_key_format   = "der"
+#   key_type             = "rsa"
+#   key_bits             = 4096
+#   exclude_cn_from_sans = true
+#   ou                   = "APJ SE"
+#   organization         = "Hashicorp Demo Org"
+# }
+# resource "vault_pki_secret_backend_config_urls" "config_urls" {
+#   backend                 = vault_mount.pki_root.path
+#   issuing_certificates    = ["${var.vault_url}/v1/${vault_mount.pki_root.path}/ca"]
+#   crl_distribution_points = ["${var.vault_url}/v1/${vault_mount.pki_root.path}/crl"]
+# }
+# //pki intermediate CA secret engine
+# resource "vault_mount" "pki_intermediate" {
+#   depends_on                = [vault_pki_secret_backend_root_cert.self-signing-cert]
+#   path                      = "pki_intermediate"
+#   type                      = "pki"
+#   default_lease_ttl_seconds = 2678400  //Default expiry of the certificates signed by this CA - 31 days
+#   max_lease_ttl_seconds     = 24819200 //Max expiry of the certificates signed by this CA - 13 Months
+# }
+# resource "vault_pki_secret_backend_intermediate_cert_request" "intermediate" {
+#   depends_on  = [vault_pki_secret_backend_root_cert.self-signing-cert]
+#   backend     = vault_mount.pki_intermediate.path
+#   type        = "internal"
+#   common_name = "apj-ca.hashicorp.demo"
+# }
+# resource "vault_pki_secret_backend_root_sign_intermediate" "root" {
+#   depends_on           = [vault_pki_secret_backend_root_cert.self-signing-cert, vault_pki_secret_backend_root_cert.self-signing-cert]
+#   backend              = vault_mount.pki_root.path
+#   csr                  = vault_pki_secret_backend_intermediate_cert_request.intermediate.csr
+#   ttl                  = 3600 * 24 * 31 * 12 * 2 //2 Years
+#   common_name          = "apj-ca.hashicorp.demo"
+#   exclude_cn_from_sans = true
+#   ou                   = "APJ SE"
+#   organization         = "Hashicorp Demo Org"
+# }
+
+# resource "vault_pki_secret_backend_intermediate_set_signed" "intermediate" {
+# depends_on = [vault_pki_secret_backend_config_urls.config_urls_int]
+#   backend     = vault_mount.pki_intermediate.path
+#   certificate = vault_pki_secret_backend_root_sign_intermediate.root.certificate
+# }
+
+# resource "vault_pki_secret_backend_config_urls" "config_urls_int" {
+#   backend                 = vault_mount.pki_intermediate.path
+#   issuing_certificates    = ["${var.vault_url}/v1/${vault_mount.pki_intermediate.path}/ca"]
+#   crl_distribution_points = ["${var.vault_url}/v1/${vault_mount.pki_intermediate.path}/crl"]
+# }
