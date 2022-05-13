9a77e2d184f2fd1a7d1141289a64406dab479cdb
Yulei Liu <yulei.liu@gmail.com>
2022-05-13 13:49:07 +1000

    reorg

--- pki-secrets-roles.tf
+++ pki-secrets-roles.tf
@@ -1,6 +1,6 @@
 locals {
   # Take a directory of YAML files, read each one that matches naming pattern and bring them in to Terraform's native data set
-  inputpkirolevars = [for f in fileset(path.module, "pkiroles/{pkirole}*.yaml") : yamldecode(file(f))]
+  inputpkirolevars = [for f in fileset(path.module, "pki-secrets-roles/{pkirole}*.yaml") : yamldecode(file(f))]
   # Take that data set and format it so that it can be used with the for_each command by converting it to a map where each top level key is a unique identifier.
   # In this case I am using the name key from my example YAML files
   inputpkirolemap = { for pkirole in toset(local.inputpkirolevars) : pkirole.name => pkirole }
