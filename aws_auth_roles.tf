cdb5bf2ed067eb128e3d41e93699d59805bc3927
Yulei Liu <yulei.liu@gmail.com>
2022-09-15 16:50:14 +1000

    fmt

--- aws_auth_roles.tf
+++ aws_auth_roles.tf
@@ -28,34 +28,3 @@ resource "vault_aws_auth_backend_role" "aws-role" {
     vault_auth_backend.aws
   ]
 }
-
-resource "vault_policy" "trusted-orchestrator" {
-  name   = "trusted-orchestrator"
-  policy = <<EOF
- path "pki_intermediate/issue/machine-id" {
-    capabilities = ["create", "read", "update", "delete", "list", "sudo"]
- }
- EOF
-}
-
-resource "vault_token_auth_backend_role" "trusted-orchestrator" {
-  role_name              = "trusted-orchestrator"
-  allowed_policies       = ["trusted-orchestrator"]
-  orphan                 = true
-  token_period           = "86400"
-  renewable              = true
-  token_explicit_max_ttl = "115200"
-  path_suffix            = "trusted-orchestrator"
-}
-
-resource "vault_token" "trusted-orchestrator" {
-  role_name = "trusted-orchestrator"
-
-  policies = [vault_policy.trusted-orchestrator.name, "default"]
-
-  renewable = true
-  ttl       = "728h"
-
-  renew_min_lease = 43200
-  renew_increment = 86400
-}
