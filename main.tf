6daee255384b6886d4d213655a46d0aeb0b95752
Yulei Liu <yulei.liu@gmail.com>
2022-09-15 22:18:09 +1000

    sentinel fmt

--- main.tf
+++ main.tf
@@ -250,27 +250,28 @@ resource "vault_egp_policy" "only-allow-machines-to-request-their-own-id" {
 
   policy = <<EOT
 entity_is_trusted_orchestrator = rule {
-    token.display_name is "token-trusted-orchestrator"
+	token.display_name is "token-trusted-orchestrator"
 }
 
 entity_name_match_request = rule {
-  identity.entity.aliases[0].name is request.data.common_name
+	identity.entity.aliases[0].name is request.data.common_name
 }
 
 if entity_is_trusted_orchestrator {
-    print("trace:Request.data:",request.data)
-    print("trace:Token.display_name:",token.display_name)
+	print("trace:Request.data:", request.data)
+	print("trace:Token.display_name:", token.display_name)
 } else {
-      print("trace:Request.data:",request.data)
-      print("trace:identity.entity.name:",identity.entity.name)
-  if not entity_name_match_request {
-    print("Requestors entity name ",identity.entity.aliases[0].name, " does not match requested machine id ",request.data.common_name)
-  }
+	print("trace:Request.data:", request.data)
+	print("trace:identity.entity.name:", identity.entity.name)
+	if not entity_name_match_request {
+		print("Requestors entity name ", identity.entity.aliases[0].name, " does not match requested machine id ", request.data.common_name)
+	}
 }
 
 main = rule {
-    (entity_is_trusted_orchestrator or entity_name_match_request) 
+	(entity_is_trusted_orchestrator or entity_name_match_request)
 }
+
 EOT
 }
 
