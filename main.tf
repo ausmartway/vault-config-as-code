68559976825c516bf010832d3dca6a5dd356045f
Yulei Liu <yulei.liu@gmail.com>
2022-09-19 09:24:55 +1000

    enable egp

--- main.tf
+++ main.tf
@@ -243,37 +243,37 @@ output "trusted-orchestrator" {
   sensitive = true
 }
 
-# resource "vault_egp_policy" "only-allow-machines-to-request-their-own-id" {
-#   name              = "only-allow-machines-to-request-their-own-id"
-#   paths             = ["pki_intermediate/issue/machine-id"]
-#   enforcement_level = "hard-mandatory"
+resource "vault_egp_policy" "only-allow-machines-to-request-their-own-id" {
+  name              = "only-allow-machines-to-request-their-own-id"
+  paths             = ["pki_intermediate/issue/machine-id"]
+  enforcement_level = "hard-mandatory"
 
-#   policy = <<EOT
-# entity_is_trusted_orchestrator = rule {
-# 	token.display_name is "token-trusted-orchestrator"
-# }
+  policy = <<EOT
+entity_is_trusted_orchestrator = rule {
+	token.display_name is "token-trusted-orchestrator"
+}
 
-# entity_name_match_request = rule {
-# 	identity.entity.aliases[0].name is request.data.common_name
-# }
+entity_name_match_request = rule {
+	identity.entity.aliases[0].name is request.data.common_name
+}
 
-# if entity_is_trusted_orchestrator {
-# 	print("trace:Request.data:", request.data)
-# 	print("trace:Token.display_name:", token.display_name)
-# } else {
-# 	print("trace:Request.data:", request.data)
-# 	print("trace:identity.entity.name:", identity.entity.name)
-# 	if not entity_name_match_request {
-# 		print("Requestors entity name ", identity.entity.aliases[0].name, " does not match requested machine id ", request.data.common_name)
-# 	}
-# }
+if entity_is_trusted_orchestrator {
+	print("trace:Request.data:", request.data)
+	print("trace:Token.display_name:", token.display_name)
+} else {
+	print("trace:Request.data:", request.data)
+	print("trace:identity.entity.name:", identity.entity.name)
+	if not entity_name_match_request {
+		print("Requestors entity name ", identity.entity.aliases[0].name, " does not match requested machine id ", request.data.common_name)
+	}
+}
 
-# main = rule {
-# 	(entity_is_trusted_orchestrator or entity_name_match_request)
-# }
+main = rule {
+	(entity_is_trusted_orchestrator or entity_name_match_request)
+}
 
-# EOT
-# }
+EOT
+}
 
 
 //Audit device
