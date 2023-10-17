d87cf60d95d7926223ef692a5297e597a1659a07
Yulei Liu <yulei.liu@gmail.com>
2023-10-17 16:33:15 +1100

    output CA certs

---
+++ output.tf
@@ -0,0 +1,12 @@
+output pki_root_cert {
+  value       = vault_pki_secret_backend_root_cert.certificate
+  sensitive   = false
+  description = "the certificate of pki_root default issuer"
+}
+
+output pki_intermediate_cert {
+  value       = vault_pki_secret_backend_root_sign_intermediate.intermediate.certificate
+  sensitive   = false
+  description = "the certificate of pki_intermediate default issuer"
+}
+
