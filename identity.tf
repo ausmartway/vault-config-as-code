b7de18828ba1ae0221fa09c54e571fdef46af040
Yulei Liu <yulei.liu@gmail.com>
2024-02-20 19:47:46 +1100

    testing Identity feature

--- identity.tf
+++ identity.tf
@@ -1,15 +1,26 @@
+resource "vault_identity_oidc_key" "human" {
+  name      = "human"
+  algorithm = "RS256"
+}
+
+resource "vault_identity_oidc_role" "human" {
+  name = "human"
+  key  = vault_identity_oidc_key.human.name
+}
+
 resource "vault_identity_entity" "yulei" {
   name     = "yulei"
-  policies = ["superuser","yulei-identity-token-policies"]
+  policies = ["superuser", "human-identity-token-policies"]
   metadata = {
-    group = "SE"
+    role = "Sales"
+    team = "SalesEngineer"
   }
 }
 
-resource "vault_policy" "yulei-identity-token-policies" {
+resource "vault_policy" "human-identity-token-policies" {
   name   = "yulei-identity-token-policies"
   policy = <<EOF
- path "identity/oidc/token/yulei" {
+ path "identity/oidc/token/human" {
    capabilities = ["create", "read", "update", "delete", "list", "sudo"]
  }
  EOF
@@ -26,3 +37,4 @@ resource "vault_identity_entity_alias" "yulei-pki" {
   mount_accessor = vault_auth_backend.cert.accessor
   canonical_id   = vault_identity_entity.yulei.id
 }
+
