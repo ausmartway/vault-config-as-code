2a0b8545d9aa51f8dcdd927ecb2b7a265ae4a50e
Yulei Liu <yulei.liu@gmail.com>
2024-02-26 12:44:59 +1100

    syntax

--- application_identities.tf
+++ application_identities.tf
@@ -35,7 +35,7 @@ resource "vault_identity_oidc_role" "application_identity" {
   name = "application_identity"
   template = jsonencode(
     {
-      "azp" = "spiffe://vault/{{identity.entity.metadata.enviroment}}/{{identity.entity.metadata.business_unit}}/{{identity.entity.name}}"
+      "azp" : "spiffe://vault/{{identity.entity.metadata.enviroment}}/{{identity.entity.metadata.business_unit}}/{{identity.entity.name}}"
     }
   )
   client_id = "spiffe://vault"
