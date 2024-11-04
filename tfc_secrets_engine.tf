80659d42709012b4061d3809210dd84f161a1345
Yulei Liu <yulei.liu@gmail.com>
2024-11-04 17:04:17 +1100

    enable tfc secrets engine

---
+++ tfc_secrets_engine.tf
@@ -0,0 +1,9 @@
+variable "tfc_token" {
+    description = "A TFC/TFE token that is used for management purposes"
+}
+
+resource "vault_terraform_cloud_secret_backend" "terraform_cloud" {
+    backend     = "terraform"
+    description = "Manages the Terraform Cloud backend"
+    token = var.tfc_token
+}
