b5f0d64c3925a0d88529308292504ee145ff7d78
Yulei Liu <yulei.liu@gmail.com>
2025-01-10 21:30:10 +1100

    add pre-commit checks

--- versions.tf
+++ versions.tf
@@ -1,10 +1,21 @@
 terraform {
-  required_version = ">= 1.10"
+  required_version = "1.10.4"
 
   required_providers {
     # Update these to reflect the actual requirements of your module
     vault = {
-      version = ">= 4.5.0"
+      source  = "hashicorp/vault"
+      version = "~> 4.5.0"
+    }
+
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.12.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.6"
     }
   }
 }
