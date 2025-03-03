2b33ad29dfd7b9ee56c456fe73f2c4799c84f4e7
Yulei Liu <yulei.liu@gmail.com>
2025-03-03 13:50:07 +1100

    accomodate provider bug

--- pkiroles.tf
+++ pkiroles.tf
@@ -24,13 +24,15 @@ resource "vault_pki_secret_backend_role" "role" {
   allow_any_name     = false
   allow_ip_sans      = false
   require_cn         = true
-  ou                 = each.value.ou
-  organization       = each.value.organization
-  country            = each.value.country
-  locality           = each.value.locality
-  province           = each.value.province
-  street_address     = each.value.street_address
-  postal_code        = each.value.postal_code
+
+  // the type of below fields should be just string, instead of list(string), I will fix it later
+  ou             = [each.value.ou]
+  organization   = [each.value.organization]
+  country        = [each.value.country]
+  locality       = [each.value.locality]
+  province       = [each.value.province]
+  street_address = [each.value.street_address]
+  postal_code    = [each.value.postal_code]
 
   depends_on = [
     vault_mount.pki_intermediate
