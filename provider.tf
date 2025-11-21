0ff11e404e99ef8725485800c8e67d4937893d2d
Yulei Liu <yulei.liu@gmail.com>
2025-11-21 13:58:40 +1100

    enable WIF feature for Vault provider

--- provider.tf
+++ provider.tf
@@ -13,5 +13,12 @@
 #   - Cloud provider workload identity (AWS IAM, GCP, Azure)
 #   - Terraform Cloud dynamic credentials
 provider "vault" {
+  // skip_child_token must be explicitly set to true as HCP Terraform manages the token lifecycle
+  skip_child_token = true
+  address          = var.tfc_vault_dynamic_credentials.default.address
+  namespace        = var.tfc_vault_dynamic_credentials.default.namespace
 
+  auth_login_token_file {
+    filename = var.tfc_vault_dynamic_credentials.default.token_filename
+  }
 }
--- variables.tf
+++ variables.tf
@@ -36,3 +36,21 @@ variable "environment" {
 #   type        = string
 #   description = "Azure subscription id"
 # }
+
+variable "tfc_vault_dynamic_credentials" {
+  description = "Object containing Vault dynamic credentials configuration"
+  type = object({
+    default = object({
+      token_filename = string
+      address        = string
+      namespace      = string
+      ca_cert_file   = string
+    })
+    aliases = map(object({
+      token_filename = string
+      address        = string
+      namespace      = string
+      ca_cert_file   = string
+    }))
+  })
+}
