344034ce8c9aee1d9ab367f228291c721e22ef38
Yulei Liu <yulei.liu@gmail.com>
2025-12-10 12:44:50 +1100

    test

--- provider.tf
+++ provider.tf
@@ -16,7 +16,7 @@ provider "vault" {
   // skip_child_token must be explicitly set to true as HCP Terraform manages the token lifecycle
   skip_child_token = true
   address          = var.tfc_vault_dynamic_credentials.default.address
-  namespace        = var.tfc_vault_dynamic_credentials.default.namespace
+  # namespace        = var.tfc_vault_dynamic_credentials.default.namespace
 
   auth_login_token_file {
     filename = var.tfc_vault_dynamic_credentials.default.token_filename
